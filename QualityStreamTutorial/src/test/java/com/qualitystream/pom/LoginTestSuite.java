387a8e997883910d9cdd4eee10fd54e1fe8a7559
Angelo Huaraca <angelohb1203@gmail.com>
2019-08-05 23:40:59 -0500

    primer commit del tutorial selenium

---
+++ QualityStreamTutorial/src/test/java/com/qualitystream/pom/LoginTestSuite.java
@@ -0,0 +1,11 @@
+package com.qualitystream.pom;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LoginLogoutTest.class, LoginWelcomeTest.class })
+public class LoginTestSuite {
+
+}
---
+++ QualityStreamTutorial/src/test/java/com/qualitystream/pom/LoginWelcomeTest.java
@@ -0,0 +1,33 @@
+package com.qualitystream.pom;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class LoginWelcomeTest {
+	
+	private WebDriver driver;
+	LoginWelcomePage login_welcome_page;
+	
+	@Before
+	public void setUp() throws Exception {
+		login_welcome_page = new LoginWelcomePage(driver);
+		driver = login_welcome_page.chromeDriverConnection();
+		login_welcome_page.visit("file:///home/ahuaracab/Desktop/web/curso-javascript/proyecto-js/index.html");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		login_welcome_page.LoginWelcome();
+		Thread.sleep(3000);
+		assertEquals("Bienvenido, angelo", login_welcome_page.WelcomeMessage());
+	}
+
+}
